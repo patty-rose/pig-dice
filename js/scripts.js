4f11bd0890a2ffba3fba8a1b7506f431118adf8b
patty-rose <oteropatty@gmail.com>
2022-05-03 18:34:14 -0700

    added game object and updated styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
 //Business Logic
+//Game constructor and methods:
+function Game(playerOne, playerTwo){
+  this.playerOne = playerOne;
+  this.playerTwo = playerTwo;
+  this.activePlayer;
+}
+
+Game.prototype.switchActivePlayer = function() {
+  if(this.playerOne.hasDice === true) {
+    this.playerOne.hasDice = false;
+    this.playerTwo.hasDice = true;
+    this.activePlayer = this.playerTwo;
+  } else if (this.playerTwo.hasDice === true) {
+    this.playerTwo.hasDice = false;
+    this.playerOne.hasDice = true;
+    this.activePlayer = this.playerOne
+  } else {
+    this.activePlayer = this.playerOne;
+    this.playerOne.hasDice = true;
+  }
+};
+
+//Player constructor and methods:
 function Player(playerName) {
   this.playerName = playerName;
+  this.hasDice = false;
   this.scoreTotal = 0;
   this.currentTotal = 0;
   this.currentRoll = 0;
@@ -19,22 +43,133 @@ Player.prototype.currentScoreAdd = function() {
 Player.prototype.holdScore = function () {
   this.scoreTotal += this.currentTotal;
   this.currentTotal = 0;
-  if (this.scoreTotal >= 100) {
-    console.log(this.playerName + " WINS!!")
-  };
+  // if (this.scoreTotal >= 100) {
+  //   console.log(this.playerName + " WINS!!")
+  // };
 }
 
 //User Interface Logic
-let player1 = new Player;
-let player2 = new Player;
+function switchCurrentPlayer(game){
+  if(currentGame.playerOne.hasDice === true) {
+    $("#player1PlayField").hide();
+    $("#player2PlayField").show();
+    currentGame.activePlayer = playerTwo;
+    currentGame.playerTwo.hasDice = true;
+    currentGame.playerOne.hasDice = false;
+  } else if (currentGame.playerTwo.hasDice === true) {
+    $("#player1PlayField").show();
+    $("#player2PlayField").hide();
+    currentGame.activePlayer = playerOne;
+    currentGame.playerOne.hasDice = true;
+    currentGame.playerTwo.hasDice = false;
+  } else {
+    currentGame.playerOne.hasDice = true;
+    $("#player1PlayField").hide();
+    $("#player2PlayField").show();
+    currentGame.activePlayer = playerOne;
+  }
+};
+
+function rollButton(currentGame) {
+  if (currentGame.activePlayer === playerOne) {
+    currentGame.playerOne.rollDie();
+    if (currentGame.playerOne.currentRoll === 1) {
+      $(".roll").hide();
+    };
+    playerOne.currentScoreAdd();
+    $("#playerOneCurrentRoll").show();
+    $("#playerOneCurrentTotal").show();
+    $("#playerOneCurrentRoll").text(" " + currentGame.playerOne.currentRoll);
+    $("#playerOneCurrentTotal").text(" " + currentGame.playerOne.currentTotal);
+  } else if (currentGame.activePlayer === playerTwo) {
+    currentGame.playerTwo.rollDie();
+    if (currentGame.playerTwo.currentRoll === 1) {
+      $(".roll").hide();
+    };
+    currentGame.playerTwo.currentScoreAdd();
+    $("#playerTwoCurrentRoll").show();
+    $("#playerTwoCurrentTotal").show();
+    $("#playerTwoCurrentRoll").text(" " + currentGame.playerTwo.currentRoll);
+    $("#playerTwoCurrentTotal").text(" " + currentGame.playerTwo.currentTotal);
+  }
+};
+
+function displayDice(currentGame) {
+  $(".dice").hide();
+  if (currentGame.activePlayer === playerOne) {
+    if (currentGame.playerOne.currentRoll === 1) {
+      $("#one").show();
+    } else if (currentGame.playerOne.currentRoll === 2) {
+      $("#two").show();
+    } else if (currentGame.playerOne.currentRoll === 3) {
+      $("#three").show();
+    } else if (currentGame.playerOne.currentRoll === 4) {
+      $("#four").show();
+    } else if (currentGame.playerOne.currentRoll === 5) {
+      $("#five").show();
+    } else if (currentGame.playerOne.currentRoll === 6) {
+      $("#six").show();
+    };
+  } else if (currentGame.activePlayer === playerTwo){
+    if (currentGame.playerTwo.currentRoll === 1) {
+      $("#playerTwoOne").show();
+    } else if (currentGame.playerTwo.currentRoll === 2) {
+      $("#playerTwoTwo").show();
+    } else if (currentGame.playerTwo.currentRoll === 3) {
+      $("#playerTwoThree").show();
+    } else if (currentGame.playerTwo.currentRoll === 4) {
+      $("#playerTwoFour").show();
+    } else if (currentGame.playerTwo.currentRoll === 5) {
+      $("#playerTwoFive").show();
+    } else if (currentGame.playerTwo.currentRoll === 6) {
+      $("#playerTwoSix").show();
+    };
+  }
+}
+
+function holdButton(currentGame) {
+  if (currentGame.activePlayer === playerOne){
+    currentGame.playerOne.holdScore();
+    $(".roll").show();
+    $("#playerOneScoreTotal").text(" " + currentGame.playerOne.scoreTotal);
+    $("#playerOneCurrentRoll").hide();
+    $("#playerOneCurrentTotal").hide();
+    $("#player1PlayField").hide();
+    $("#player2PlayField").show();
+    if (currentGame.playerOne.scoreTotal >= 100) {
+      $(".playerOneWin").show();
+      $("#player1PlayField").hide();
+      $("#player2PlayField").hide();
+      $("#newGame").show();
+    };
+  } else {
+    currentGame.playerTwo.holdScore();
+    $(".roll").show();
+    $("#playerTwoScoreTotal").text(" " + currentGame.playerTwo.scoreTotal);
+    $("#playerTwoCurrentRoll").hide();
+    $("#playerTwoCurrentTotal").hide();
+    $("#player1PlayField").show();
+    $("#player2PlayField").hide();
+    if (currentGame.playerTwo.scoreTotal >= 100) {
+      $(".playerTwoWin").show();
+      $("#player1PlayField").hide();
+      $("#player2PlayField").hide();
+      $("#newGame").show();
+    };
+  };
+}
 
 $(document).ready(function() {
-  $("#playerOneScoreTotal").text(" " + player1.scoreTotal);
-  $("#playerTwoScoreTotal").text(" " + player2.scoreTotal);
-  $("#playerOneCurrentRoll").text(" " + player1.currentRoll);
-  $("#playerTwoCurrentRoll").text(" " + player2.currentRoll);
-  $("#playerOneCurrentTotal").text(" " + player1.currentTotal);
-  $("#playerTwoCurrentTotal").text(" " + player2.currentTotal);
+  playerOne = new Player("player 1");
+  playerTwo = new Player("player 2");
+  currentGame = new Game (playerOne, playerTwo);
+
+  $("#playerOneScoreTotal").text(" " + currentGame.playerOne.scoreTotal);
+  $("#playerTwoScoreTotal").text(" " + currentGame.playerTwo.scoreTotal);
+  $("#playerOneCurrentRoll").text(" " + currentGame.playerOne.currentRoll);
+  $("#playerTwoCurrentRoll").text(" " + currentGame.playerTwo.currentRoll);
+  $("#playerOneCurrentTotal").text(" " + currentGame.playerOne.currentTotal);
+  $("#playerTwoCurrentTotal").text(" " + currentGame.playerTwo.currentTotal);
 
   $("#rules").click(function() {
     $(".rulesText").toggle();
@@ -42,6 +177,7 @@ $(document).ready(function() {
 
   $("#startGame").click(function(event) {
     event.preventDefault();
+    switchCurrentPlayer(currentGame);
     $("#startGame").hide();
     $("#playerOneCurrentRoll").hide();
     $("#playerOneCurrentTotal").hide();
@@ -49,99 +185,25 @@ $(document).ready(function() {
     $("#playerTwoCurrentTotal").hide();
     $(".playerOneWin").hide();
     $(".playerTwoWin").hide();
-    $("#player1PlayField").toggle();
-    $("#player2PlayField").hide();
-  });
-
-  $("#playerOneRoll").click(function(event) {
-    event.preventDefault();
-    $("#playerOneCurrentRoll").show();
-    $("#playerOneCurrentTotal").show();
-    player1.rollDie();
-    $(".dice").hide();
-    if (player1.currentRoll === 1) {
-      $("#playerOneRoll").hide();
-      $("#one").show();
-    } else if (player1.currentRoll === 2) {
-      $("#two").show();
-    } else if (player1.currentRoll === 3) {
-      $("#three").show();
-    } else if (player1.currentRoll === 4) {
-      $("#four").show();
-    } else if (player1.currentRoll === 5) {
-      $("#five").show();
-    } else if (player1.currentRoll === 6) {
-      $("#six").show();
-    };
-    player1.currentScoreAdd();
-    $("#playerOneCurrentRoll").text(" " + player1.currentRoll);
-    $("#playerOneCurrentTotal").text(" " + player1.currentTotal);
-  });
-
-  $("#playerOneHold").click(function(event) {
-    event.preventDefault();
-    player1.holdScore();
-    $(".dice").hide();
-    $("#playerOneRoll").show();
-    $("#player1PlayField").hide();
-    $("#player2PlayField").show();
-    $("#playerOneScoreTotal").text(" " + player1.scoreTotal);
-    $("#playerOneCurrentRoll").hide();
-    $("#playerOneCurrentTotal").hide();
-    if (player1.scoreTotal >= 20) {
-      $(".playerOneWin").show();
-      $("#player1PlayField").hide();
-      $("#player2PlayField").hide();
-      $("#newGame").show();
-    };
-  });
-
-  $("#playerTwoRoll").click(function(event) {
-    event.preventDefault();
-    $("#playerTwoCurrentRoll").show();
-    $("#playerTwoCurrentTotal").show();
-    player2.rollDie();
-    $(".dice").hide();
-    if (player2.currentRoll === 1) {
-      $("#playertwoRoll").hide();
-      $("#playerTwoOne").show();
-    } else if (player2.currentRoll === 2) {
-      $("#playerTwoTwo").show();
-    } else if (player2.currentRoll === 3) {
-      $("#playerTwoThree").show();
-    } else if (player2.currentRoll === 4) {
-      $("#playerTwoFour").show();
-    } else if (player2.currentRoll === 5) {
-      $("#playerTwoFive").show();
-    } else if (player2.currentRoll === 6) {
-      $("#playerTwoSix").show();
-    };
-    player2.currentScoreAdd();
-    $("#playerTwoCurrentRoll").text(" " + player2.currentRoll);
-    $("#playerTwoCurrentTotal").text(" " + player2.currentTotal);
-  });
-
-  $("#playerTwoHold").click(function(event) {
-    event.preventDefault();
-    player2.holdScore();
-    $(".dice").hide();
-    $("#playerTwoRoll").show();
     $("#player1PlayField").show();
     $("#player2PlayField").hide();
-    $("#playerTwoScoreTotal").text(" " + player2.scoreTotal);
-    $("#playerTwoCurrentRoll").hide();
-    $("#playerTwoCurrentTotal").hide();
-    if (player2.scoreTotal >= 20) {
-      $(".playerTwoWin").show();
-      $("#player1PlayField").hide();
-      $("#player2PlayField").hide();
-      $("#newGame").show();
-    };
+  });
+
+  $(".roll").click(function(event) {
+    event.preventDefault();
+    rollButton(currentGame);
+    displayDice(currentGame);
+  });
+
+  $(".hold").click(function(event) {
+    event.preventDefault();    
+    holdButton(currentGame);
+    switchCurrentPlayer(currentGame);
+    $(".dice").hide();
   });
 
   $('#newGame').click(function(event) {
     event.preventDefault();
     location.reload();
-});
-
+  })
 })
